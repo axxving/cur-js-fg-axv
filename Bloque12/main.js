95284b7b5d509cbdd7a0ece2a233d8680968df9a
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-04-11 12:35:03 -0600

    feat(proyecto12): versión no funcional
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

---
+++ Bloque12/main.js
@@ -0,0 +1,28 @@
+        // Seleccion entre peliculas o series
+        // Input para escribir el titulo
+        // Boton para iniciar la busqueda
+        // Lista para ver todos los hayazgos
+        // Json con Peliculas y con Series
+        // Cada uno con 5 peliculas
+        // Buscador con 
+        // - select
+        // - escuchador de eventos para el evento change de un select
+        // - evento que muestre un alert 
+        // - el input solo admite letras, espacio y borrar 
+        // - iniciar la busqueda
+        // - ul para mostrar los resultados
+        //------------------------------------------------------------
+        // - la busqueda debera
+        // - obtener los datos del json 
+        // - verificar el array y obtener coincidencias
+        // totales o parciales con startWith() y aplica
+        // toUpperCase() para neutralizar minusculas
+        // - En cada iteracion verificar que "nombre"
+        // coincida con el ingreso del usuario
+        // - Si hay coincidencia generar una etiqueta p
+        // para almacenar la sinopsis y un li para almacenar
+        // la sinopsis y un li para almacenar el nombre, la
+        // etiqueta p se adjuntara a la etiqueta li 
+        // y este la etiqueta ul
+        // 2 eventos para la etiqueta li: mauseover y mauseout
+        // para mostrar y ocultar la sinopsis
